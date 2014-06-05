aec7c6d4766d084fde4e29c96734bcfe398f05c7
cretzuwgz <bboy_alex93@yahoo.com>
2014-06-05 20:58:57 +0300

    Add home image and update app icon

--- app/src/main/java/com/pizzatime/app/Home.java
+++ app/src/main/java/com/pizzatime/app/Home.java
@@ -33,7 +33,6 @@ public class Home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-
         return rootView;
     }
 
